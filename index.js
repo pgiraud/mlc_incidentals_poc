3a9e5c8066be1afa43b033f24551f720bed8ad60
Pierre GIRAUD <pierre.giraud@dalibo.com>
2021-05-29 10:11:28 +0200

    Display mammals data in table

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    geojson: {},
+    fields: ['Common_nam', 'Date', 'Weather']
+  },
+  computed: {
+    featuresData: function() {
+      return _.map(this.geojson.features, (feature) => {
+        return feature.properties;
+      });
+    }
+  },
+  mounted: function() {
+    this.loadData();
+  },
+  methods: {
+    loadData: function() {
+      axios
+        .get('data/mammals_4.json')
+        .then(response => (this.geojson = response.data))
+    }
   }
 })
